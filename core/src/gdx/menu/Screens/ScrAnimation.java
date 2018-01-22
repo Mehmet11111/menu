80f9c96bbd7dc4aef2ba336f9292af747063f14d
simpa4988 <simpa4988@FHC-IC0034096.wrdsb.ca>
2018-01-22 12:39:01 -0500

    Grondin

---
+++ core/src/gdx/menu/Screens/ScrAnimation.java
@@ -0,0 +1,133 @@
+package gdx.menu.Screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import gdx.menu.GamMenu;
+
+
+
+public class ScrAnimation implements Screen {
+    GamMenu game;
+    SpriteBatch batch;
+    Sprite sprguy, sprWleft, sprWright, sprWup, sprWdown, sprWLU, sprWRU, sprWLD, sprWRD;
+    Texture Txstand, TxWdown, TxWright, TxWleft, TxWup, TxWLU, TxWRU, TxWLD, TxWRD;
+
+    public ScrAnimation(GamMenu _game) {
+        game = _game;
+        batch = new SpriteBatch();
+        Txstand = new Texture("guystanding.png");
+        TxWdown = new Texture("walkingdown.png");
+        TxWright = new Texture("walkingright.png");
+        TxWleft = new Texture("walkingleft.png");
+        TxWup = new Texture("walkingup.png");
+        TxWLU = new Texture("walkingLU.png");
+        TxWRU = new Texture("walkingRU.png");
+        TxWLD = new Texture("walkingLD.png");
+        TxWRD = new Texture("walkingRD.png");
+        sprguy = new Sprite(Txstand);
+        sprWleft = new Sprite(TxWleft);
+        sprWright = new Sprite(TxWright);
+        sprWup = new Sprite(TxWup);
+        sprWdown = new Sprite(TxWdown);
+        sprWLU = new Sprite(TxWLU);
+        sprWRU = new Sprite(TxWRU);
+        sprWLD = new Sprite(TxWLD);
+        sprWRD = new Sprite(TxWRD);
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 1, 1, 0);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(sprguy, sprguy.getX(), sprguy.getY());
+        batch.end();
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+            sprguy.setX(sprguy.getX() - 200 * Gdx.graphics.getDeltaTime());
+            sprguy.setTexture(TxWleft);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+            sprguy.setX(sprguy.getX() + 200 * Gdx.graphics.getDeltaTime());
+            sprguy.setTexture(TxWright);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+            sprguy.setY(sprguy.getY() + 200 * Gdx.graphics.getDeltaTime());
+            sprguy.setTexture(TxWup);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+            sprguy.setY(sprguy.getY() - 200 * Gdx.graphics.getDeltaTime());
+            sprguy.setTexture(TxWdown);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.S)&&Gdx.input.isKeyPressed(Input.Keys.A)) {
+            sprguy.setY(sprguy.getY() - 60 * Gdx.graphics.getDeltaTime());
+            sprguy.setX(sprguy.getX() - 60 * Gdx.graphics.getDeltaTime());
+            sprguy.setTexture(TxWLD);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.S)&&Gdx.input.isKeyPressed(Input.Keys.D)) {
+            sprguy.setY(sprguy.getY() - 60 * Gdx.graphics.getDeltaTime());
+            sprguy.setX(sprguy.getX() + 60 * Gdx.graphics.getDeltaTime());
+            sprguy.setTexture(TxWRD);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.W)&&Gdx.input.isKeyPressed(Input.Keys.A)) {
+            sprguy.setY(sprguy.getY() + 60 * Gdx.graphics.getDeltaTime());
+            sprguy.setX(sprguy.getX() - 60 * Gdx.graphics.getDeltaTime());
+            sprguy.setTexture(TxWLU);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.W)&&Gdx.input.isKeyPressed(Input.Keys.D)) {
+            sprguy.setY(sprguy.getY() + 60 * Gdx.graphics.getDeltaTime());
+            sprguy.setX(sprguy.getX() + 60 * Gdx.graphics.getDeltaTime());
+            sprguy.setTexture(TxWRU);
+        }
+        if (!Gdx.input.isKeyPressed(Input.Keys.W)&&!Gdx.input.isKeyPressed(Input.Keys.A)&&!Gdx.input.isKeyPressed(Input.Keys.S)&&!Gdx.input.isKeyPressed(Input.Keys.D)) {
+            sprguy.setTexture(Txstand);
+        }   
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        Txstand.dispose();
+        TxWleft.dispose();
+        TxWright.dispose();
+        TxWup.dispose();
+        TxWdown.dispose();
+        TxWLD.dispose();
+        TxWRD.dispose();
+        TxWLU.dispose();
+        TxWRU.dispose();
+    }
+
+    @Override
+    public void show() {
+        
+    }
+
+   
+    @Override
+    public void resize(int width, int height) {
+         
+    }
+
+    @Override
+    public void pause() {
+        
+    }
+
+    @Override
+    public void resume() {
+         
+    }
+
+    @Override
+    public void hide() {
+    
+    }
+
+    
+}
---
+++ core/src/gdx/menu/Screens/ScrPlay.java
@@ -0,0 +1,174 @@
+//package gdx.menu.Screens;
+//
+//import com.badlogic.gdx.Gdx;
+//import com.badlogic.gdx.InputProcessor;
+//import com.badlogic.gdx.Screen;
+//import com.badlogic.gdx.graphics.GL20;
+//import com.badlogic.gdx.graphics.g2d.Sprite;
+//import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+//import com.badlogic.gdx.graphics.Texture;
+//import com.badlogic.gdx.graphics.OrthographicCamera;
+//import com.badlogic.gdx.Input;
+//import gdx.menu.GamMenu;
+//
+//public class ScrPlay implements Screen, InputProcessor {
+//    Dude dud1;
+//    Button btnQuit, btnAni;
+//    Wall[] arWall = new Wall[4];
+//    GamMenu gamMenu;
+//    OrthographicCamera oc;
+//    SpriteBatch batch;
+//    Texture txNamP, txWall;
+//    Sprite sprNamP;
+//
+//    public ScrPlay(GamMenu _gamMenu) {  //Referencing the main class.
+//        gamMenu = _gamMenu;
+//    }
+//
+//    @Override
+//    public void show() {
+//        oc = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+//        oc.setToOrtho(true, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+//        oc.update();
+//        txWall = new Texture("Wall.jpg");
+//        //Setting up Walls
+//        arWall[0] = new Wall(Gdx.graphics.getWidth(), 50, 0, 0);    //Top Wall
+//        arWall[1] = new Wall(50, Gdx.graphics.getHeight() - 50, Gdx.graphics.getWidth() - 50, 0);   //Right Wall
+//        arWall[2] = new Wall(50, Gdx.graphics.getHeight() - 50, 0, 0);     //Left Wall
+//        arWall[3] = new Wall(Gdx.graphics.getWidth(), 50, 0, Gdx.graphics.getHeight() - 100);       //Bottom Wall
+//        batch = new SpriteBatch();
+//        txNamP = new Texture("P.jpg");
+//        sprNamP = new Sprite(txNamP);
+//        sprNamP.setSize(60, 80);
+//        sprNamP.setFlip(false, true);
+//        sprNamP.setPosition(Gdx.graphics.getWidth() / 2 - 30, Gdx.graphics.getHeight() / 2 - 40);
+//        dud1 = new Dude(50, 100, 200, 250);
+//        btnAni = new Button(100, 50, Gdx.graphics.getWidth() - 100, Gdx.graphics.getHeight() - 50, "Animation.jpg");
+//        btnQuit = new Button(100, 50, 0, Gdx.graphics.getHeight() - 50, "Quit.jpg");
+//        Gdx.input.setInputProcessor(this);
+//    }
+//
+//    @Override
+//    public void render(float delta) {
+//        Gdx.gl.glClearColor(.135f, .206f, .235f, 1); //blue background.
+//        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+//        float fSx = dud1.getX();
+//        float fSy = dud1.getY();
+//        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+//            dud1.setX(dud1.getX() - 5);
+//        }
+//        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+//            dud1.setX(dud1.getX() + 5);
+//        }
+//        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+//            dud1.setY(dud1.getY() + 5);
+//        }
+//        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+//            dud1.setY(dud1.getY() - 5);
+//        }
+//        if (isHitS(dud1, sprNamP)) {
+//            dud1.setPosition(fSx, fSy);
+//        }
+//        for (int i = 0; i < arWall.length; i++) {
+//            if (isHitS(dud1, arWall[i])) {
+//                dud1.setPosition(fSx, fSy);
+//            }
+//        }
+//        batch.begin();
+//        batch.setProjectionMatrix(oc.combined);
+//        btnAni.draw(batch);
+//        btnQuit.draw(batch);
+//        sprNamP.draw(batch);
+//        dud1.draw(batch);
+//        for (int i = 0; i < arWall.length; i++) {
+//            arWall[i].draw(batch);
+//        }
+//        batch.end();
+//
+//    }
+//
+//    @Override
+//    public void resize(int width, int height) {
+//    }
+//
+//    @Override
+//    public void pause() {
+//    }
+//
+//    @Override
+//    public void resume() {
+//    }
+//
+//    @Override
+//    public void hide() {
+//    }
+//
+//    @Override
+//    public void dispose() {
+//        batch.dispose();
+//        txNamP.dispose();
+//    }
+//
+//    @Override
+//    public boolean keyDown(int keycode) {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean keyUp(int keycode) {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean keyTyped(char character) {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+//        if (button == Input.Buttons.LEFT) {
+//            //System.out.println(screenX +" " + screenY);
+//            if (isHitB(screenX, screenY, btnAni)) {
+//                gamMenu.updateState(3);
+//                System.out.println("Hit Tools");
+//            } else if (isHitB(screenX, screenY, btnQuit)) {
+//                gamMenu.updateState(2);
+//                System.out.println("Hit Quit");
+//            } else {
+//            }
+//        }
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean touchDragged(int screenX, int screenY, int pointer) {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean mouseMoved(int screenX, int screenY) {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean scrolled(int amount) {
+//        return false;
+//    }
+//
+//    public boolean isHitB(int nX, int nY, Sprite sprBtn) {
+//        if (nX > sprBtn.getX() && nX < sprBtn.getX() + sprBtn.getWidth() && nY > sprBtn.getY() && nY < sprBtn.getY() + sprBtn.getHeight()) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
+//
+//    public boolean isHitS(Sprite spr1, Sprite spr2) {
+//        return spr1.getBoundingRectangle().overlaps(spr2.getBoundingRectangle());
+//    }
+//}
